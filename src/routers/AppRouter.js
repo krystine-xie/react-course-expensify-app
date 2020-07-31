a319e024cf3e2ee0232141debf6492a380755062
Krystine Xie <krystinex@gmail.com>
2020-07-31 10:12:38 -0700

    Fixed dashboard typo again

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Link, NavLink, Route, Switch } from 'react-router-dom';
 import Header from '../components/Header'; 
-import ExpenseDashboardPage from '../components/ExpenseDashBoardPage';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import HelpPage from '../components/HelpPage';
