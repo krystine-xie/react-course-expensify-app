cc0a1427cb1304dd5538e8ce93cd8ee56e728b23
Krystine Xie <krystinex@gmail.com>
2020-08-05 13:12:46 -0700

    set up startEditExpense and fixed bugs

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,11 +1,11 @@
 import React from 'react'; 
 import { connect } from 'react-redux'; 
 import ExpenseForm from './ExpenseForm';
-import { editExpense, startRemoveExpense } from '../actions/expenses';
+import { startEditExpense, startRemoveExpense } from '../actions/expenses';
 
 export class EditExpensePage extends React.Component {
     onSubmit = (expense) => {
-        this.props.editExpense(this.props.expense.id, expense);
+        this.props.startEditExpense(this.props.expense.id, expense);
         this.props.history.push('/');
     }; 
    
@@ -27,17 +27,13 @@ export class EditExpensePage extends React.Component {
     }
 }
 
-const mapStateToProps = (state, props) => {
-    return {
-        expense: state.expenses.find((expense) => {
-            return expense.id === props.match.params.id;
-        })
-    }
-}
+const mapStateToProps = (state, props) => ({
+    expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+  });
 
 const mapDispatchToProps = (dispatch) => ({
-    editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+    startEditExpense: (id, expense) => dispatch(startEditExpense(id, expense)),
     startRemoveExpense: ({id}) => dispatch(startRemoveExpense({id}))
 })
 
-export default connect(undefined, mapDispatchToProps, mapStateToProps)(EditExpensePage);
+export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
